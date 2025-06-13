bf28dcdbe217694354b6ea148431ac06f381eb9f
abmiguez <blancomiguez@gmail.com>
2025-06-13 15:52:03 +0200

    Update metaphlan.sh

--- metaphlan.sh
+++ metaphlan.sh
@@ -2,4 +2,4 @@
 sample_name=$1
 mkdir -p /data/mpa411/${sample_name}
 bzcat /data/preprocessing/${sample_name}/*.fastq.bz2 | metaphlan --input_type fastq --tmp_dir /tmp --index mpa_vJan21_TOY_CHOCOPhlAnSGB_202103 --bowtie2db /databases/mpa_database -o /data/mpa411/${sample_name}/${sample_name}_profile.tsv --force --no_map
-sudo sed -i -e 's/mpa_vJan21_TOY_CHOCOPhlAnSGB_202103/mpa_vJun23_CHOCOPhlAnSGB_202307/g' /data/mpa411/${sample_name}/${sample_name}_profile.tsv
+sed -i -e 's/mpa_vJan21_TOY_CHOCOPhlAnSGB_202103/mpa_vJun23_CHOCOPhlAnSGB_202307/g' /data/mpa411/${sample_name}/${sample_name}_profile.tsv
