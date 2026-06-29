f7544d4c106eea202f3bba09590f955ed8cbb21d
abmiguez <blancomiguez@gmail.com>
2026-06-29 12:18:03 +0200

    Update metaphlan4.2.4.sh

--- metaphlan4.2.4.sh
+++ metaphlan4.2.4.sh
@@ -3,5 +3,5 @@ sample_name=$1
 mkdir -p /output/tmp/mpa424/tmp
 echo "Getting mpa database content"
 ls /databases/
-zcat /input/${sample_name}/*.fastq.gz | metaphlan --input_type fastq --tmp_dir /output/tmp/mpa424/tmp --index mpa_vJan25_CHOCOPhlAnSGB_202503 --bowtie2db /databases/ -o /output/tmp/mpa424/${sample_name}/${sample_name}_profile.tsv --force --no_map
+zcat /input/${sample_name}/*.fastq.gz | metaphlan --input_type fastq --tmp_dir /output/tmp/mpa424/tmp --index mpa_vJan25_CHOCOPhlAnSGB_202503 --bowtie2db /databases/ -o /output/tmp/mpa424/${sample_name}/${sample_name}_profile.tsv --nproc 4 --force --no_map
 sed -i "2d" /output/tmp/mpa424/${sample_name}/${sample_name}_profile.tsv
