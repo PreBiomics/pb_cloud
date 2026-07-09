48d96bc33bc5315e4a9fb074d7e4651cc8c866e7
abmiguez <blancomiguez@gmail.com>
2026-07-09 11:15:46 +0200

    Update humann4.alpha.1.sh

--- humann4.alpha.1.sh
+++ humann4.alpha.1.sh
@@ -11,8 +11,7 @@ humann --input /output/tmp/hnn4/tmp/${sample_name}/${sample_name}.fastq \
 	--output /output/tmp/hnn4/${sample_name} --input-format fastq \
 	--nucleotide-database /databases/chocophlan --protein-database /databases/uniref \
 	--threads ${nproc} --verbose --remove-temp-output \
-	--metaphlan-options "--index mpa_vOct22_CHOCOPhlAnSGB_202403 --offline \
-	--bowtie2db /databases/mpa_database -t rel_ab_w_read_stats" \
+	--metaphlan-options "--index mpa_vOct22_CHOCOPhlAnSGB_202403 --offline --bowtie2db /databases/mpa_database -t rel_ab_w_read_stats" \
 	--utility-database /databases/utility_mapping --remove-temp-output
   
 humann_renorm_table -i /output/tmp/hnn4/${sample_name}/${sample_name}_2_genefamilies.tsv -u relab -o /output/tmp/hnn4/${sample_name}/${sample_name}_2_genefamilies_relab.tsv
