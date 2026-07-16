27722632a3ab95a4861e39398fad58c0e0d64044
Aitor <blancomiguez@gmail.com>
2026-07-16 15:00:44 +0200

    Improves preprocessing script

---
+++ preprocessing.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -euo pipefail
+
+export PATH=/output/pb_cloud/preprocessing:$PATH
+
+sample_name=${1}
+nproc=${2}
+host=${3}
+mapper=${4}
+qc=${5}
+
+preprocessing.py \
+        -m ${mapper} \
+        -i /input/${sample_name}/ \
+        -s ${sample_name} \
+        -f R1 \
+        -r R2 \
+        -n ${nproc} \
+        -t ${nproc} \
+	${host} \
+        -d /databases/kraken2_databases \
+	-x /databases/bowtie2_indexes \
+        -p \
+	-o /output/tmp/preprocessing/${sample_name} \
+	-q ${qc} \
+        --verbose &> /output/tmp/preprocessing/${sample_name}/${sample_name}_preproc.log
