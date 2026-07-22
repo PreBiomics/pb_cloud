883a65c47ceb0a8f6ed9c7667b3b8c614d1d4794
abmiguez <blancomiguez@gmail.com>
2026-07-22 15:28:27 +0200

    Update snp_indel.sh

--- snp_indel.sh
+++ snp_indel.sh
@@ -13,17 +13,16 @@ R1=/input/${SAMPLE}/${SAMPLE}_R1.fastq.gz
 R2=/input/${SAMPLE}/${SAMPLE}_R2.fastq.gz
 REFERENCE=/databases/$2
 OUTDIR=/output/tmp/wgs/${SAMPLE}
+QC="${OUTDIR}/QC"
 THREADS=$3
 
 mkdir -p "${OUTDIR}"
+mkdir -p "${QC}"
 
 SAM="${OUTDIR}/${SAMPLE}.sam"
 BAM="${OUTDIR}/${SAMPLE}.bam"
 SORTED_BAM="${OUTDIR}/${SAMPLE}.sorted.bam"
 
-GVCF="${OUTDIR}/${SAMPLE}.g.vcf.gz"
-RAW_VCF="${OUTDIR}/${SAMPLE}.raw.vcf.gz"
-FILTERED_VCF="${OUTDIR}/${SAMPLE}.filtered.vcf.gz"
 
 echo "[$(date)] Running BWA-MEM2..."
 
@@ -53,7 +52,6 @@ samtools sort \
 
 rm "${BAM}"
 
-
 echo "[$(date)] Indexing BAM..."
 
 samtools index \
@@ -61,37 +59,73 @@ samtools index \
     "${SORTED_BAM}"
 echo "[$(date)] Running GATK HaplotypeCaller..."
 
-gatk HaplotypeCaller \
+echo "[$(date)] Collecting QC metrics"
+
+# Alignment statistics
+samtools flagstat "${SORTED_BAM}" \
+    > "${QC}/${SAMPLE}.flagstat.txt"
+
+samtools stats "${SORTED_BAM}" \
+    > "${QC}/${SAMPLE}.stats.txt"
+
+# Coverage statistics
+samtools coverage "${SORTED_BAM}" \
+    > "${QC}/${SAMPLE}.coverage.txt"
+
+# Mean depth
+samtools depth -a "${SORTED_BAM}" \
+    | awk '{sum+=$3;cnt++} END {print sum/cnt}' \
+    > "${QC}/${SAMPLE}.mean_depth.txt"
+
+echo "[$(date)] BaseRecalibrator"
+
+gatk BaseRecalibrator \
     -R "${REFERENCE}.fa" \
     -I "${SORTED_BAM}" \
-    -O "${GVCF}" \
-    -ERC GVCF
+    --known-sites /databases/Homo_sapiens_assembly38.dbsnp138.vcf \
+    --known-sites /databases/Homo_sapiens_assembly38.known_indels.vcf.gz \
+    --known-sites /databases/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz \
+    -O "${OUTDIR}/${SAMPLE}.recal.table"
 
+echo "[$(date)] ApplyBQSR"
 
-echo "[$(date)] Converting GVCF to VCF..."
-
-gatk GenotypeGVCFs \
+gatk ApplyBQSR \
     -R "${REFERENCE}.fa" \
-    -V "${GVCF}" \
-    -O "${RAW_VCF}"
+    -I "${SORTED_BAM}" \
+    --bqsr-recal-file "${OUTDIR}/${SAMPLE}.recal.table" \
+    -O "${OUTDIR}/${SAMPLE}.recal.bam"
 
+samtools index "${OUTDIR}/${SAMPLE}.recal.bam"
 
-echo "[$(date)] Filtering variants..."
+echo "[$(date)] HaplotypeCaller"
 
-bcftools filter \
-    -i 'QUAL>=30 && DP>=10' \
-    "${RAW_VCF}" \
-    -Oz \
-    -o "${FILTERED_VCF}"
+gatk HaplotypeCaller \
+    -R "${REFERENCE}.fa" \
+    -I "${OUTDIR}/${SAMPLE}.recal.bam" \
+    -ERC GVCF \
+    --native-pair-hmm-threads ${THREADS} \
+    -O "${OUTDIR}/${SAMPLE}.vcf.gz"
 
-echo "[$(date)] Indexing final VCF..."
+echo "[$(date)] Select SNP and INDELs"
 
-bcftools index \
-    "${FILTERED_VCF}"
+gatk SelectVariants \
+        -R "${REFERENCE}.fa" \
+        -V ${OUTDIR}/${SAMPLE}.vcf.gz \
+        --select-type-to-include SNP \
+        -O "${OUTDIR}/${SAMPLE}.SNP.vcf.gz"
 
+gatk SelectVariants \
+        -R "${REFERENCE}.fa" \
+        -V ${OUTDIR}/${SAMPLE}.vcf.gz \
+        --select-type-to-include INDEL \
+        -O "${OUTDIR}/${SAMPLE}.INDEL.vcf.gz"
 
-echo "===================================="
-echo "Pipeline finished"
-echo "Final VCF:"
-echo "${FILTERED_VCF}"
-echo "===================================="
+echo ""
+echo "Pipeline completed successfully."
+echo ""
+echo "Output:"
+echo "  QC metrics : ${QC}"
+echo "  Recal BAM  : ${OUTDIR}/${SAMPLE}.recal.bam"
+echo "  gVCF       : ${OUTDIR}/${SAMPLE}.vcf.gz"
+echo "  SNP gVCF   : ${OUTDIR}/${SAMPLE}.SNP.vcf.gz"
+echo "  INDEL gVCF : ${OUTDIR}/${SAMPLE}.INDEL.vcf.gz"
